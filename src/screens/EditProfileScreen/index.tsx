c39768c9fd577e6ffec921ca1e09877e8f08ec95
Isabelle Hondo <isefshondo@gmail.com>
2024-04-05 09:01:30 -0300

    chore: functions of edit profile screen

--- src/screens/EditProfileScreen/index.tsx
+++ src/screens/EditProfileScreen/index.tsx
@@ -1,26 +1,73 @@
-import React from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { storageInstance } from "../../utils/storage/index.utils";
+import React from 'react';
+import {Alert, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {storageInstance} from '../../utils/storage/index.utils';
 
-export type EditProfileInputValues = { name: string | null; email: string | null };
+export type EditProfileInputValues = {
+  name: string | null;
+  email: string | null;
+  password?: string | null;
+};
 
 export const EditProfileScreen: React.FC = () => {
-    const [editProfileInputValues, setEditProfileInputValues] = React.useState<EditProfileInputValues>({
-        name: null,
-        email: null,
+  const [editProfileInputValues, setEditProfileInputValues] =
+    React.useState<EditProfileInputValues>({
+      name: null,
+      email: null,
     });
+  const [isInputFocused, setIsInputFocused] = React.useState({
+    name: false,
+    email: false,
+    password: false,
+  });
+  const iseFirstRender = React.useRef<boolean>(true);
 
-    React.useEffect(() => {
-        setEditProfileInputValues({
-            name: storageInstance.getString('name'),
-            email: storageInstance.getString('email'),
-        });
-    }, [storageInstance]);
+  React.useEffect(() => {
+    setEditProfileInputValues({
+      name: storageInstance.getString('name') ?? null,
+      email: storageInstance.getString('email') ?? null,
+    });
+  }, []);
 
-    return (
-        <SafeAreaView>
-            <Text>Edit Profile Screen</Text>
-        </SafeAreaView>
-    );
+  const handleInputState = (
+    inputName: keyof EditProfileInputValues,
+    value: string,
+  ) => {
+    if (isInputFocused[inputName]) {
+      setEditProfileInputValues(prevState => ({
+        ...prevState,
+        [inputName]: value,
+      }));
+      storageInstance.set(inputName, value);
+    }
+  };
+
+  React.useEffect(() => {
+    if (iseFirstRender.current) {
+      iseFirstRender.current = false;
+      return;
+    }
+    const requestTimer = setTimeout(async () => {
+      try {
+        // TODO: Introduce the real HTTPS URL
+        const res = await fetch('');
+        const data = await res.json();
+
+        if (res.ok) {
+          Alert.alert('Deu certo!', data.message);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }, 1500);
+    return () => clearTimeout(requestTimer);
+  }, [editProfileInputValues]);
+
+  return (
+    <SafeAreaView>
+      <Text>Edit Profile Screen</Text>
+      <Text>{editProfileInputValues.name}</Text>
+      <Text>{editProfileInputValues.email}</Text>
+    </SafeAreaView>
+  );
 };
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
-import { storageInstance } from '../../utils/storage/index.utils';
+import {storageInstance} from '../../utils/storage/index.utils';
 
 export type AnalyticsDataInfo = {
   animalsCount: number | null;
@@ -8,31 +8,39 @@ export type AnalyticsDataInfo = {
   sickAnimalsCount: number | null;
   curedAnimalsCount: number | null;
   graphics: number[] | null;
-}
+};
 
 export const HomeScreen: React.FC = () => {
   const name = storageInstance.getString('name')?.split(' ')[0];
-  const [analyticsDataInfo, setAnalyticsDataInfo] = React.useState<AnalyticsDataInfo>({
-    animalsCount: null,
-    currentPositiveCasesPercentage: null,
-    sickAnimalsCount: null,
-    curedAnimalsCount: null,
-    graphics: null,
-  });
+  const [analyticsDataInfo, setAnalyticsDataInfo] =
+    React.useState<AnalyticsDataInfo>({
+      animalsCount: null,
+      currentPositiveCasesPercentage: null,
+      sickAnimalsCount: null,
+      curedAnimalsCount: null,
+      graphics: null,
+    });
   // TODO: Change the initial value of the filter to the default selected value of the design
-  const [graphicsFilter, setGraphicsFilter] = React.useState<string | null>(null);
+  const [graphicsFilter, setGraphicsFilter] = React.useState<string | null>(
+    null,
+  );
 
   React.useEffect(() => {
     async function fetchHomeData() {
       try {
         // TODO: Introduce the real HTTPS URL
         const res = await fetch('');
-        if (!res.ok) throw new Error (`HTTP ERROR! Status: ${res.status}; Message: ${res.statusText}`)
+        if (!res.ok) {
+          throw new Error(
+            `HTTP ERROR! Status: ${res.status}; Message: ${res.statusText}`,
+          );
+        }
         const data = await res.json();
 
         setAnalyticsDataInfo({
           animalsCount: data.animals_count,
-          currentPositiveCasesPercentage: data.current_positive_cases_percentage,
+          currentPositiveCasesPercentage:
+            data.current_positive_cases_percentage,
           sickAnimalsCount: data.sick_animals_count,
           curedAnimalsCount: data.cured_animals_count,
           graphics: data.graphics,
@@ -42,7 +50,7 @@ export const HomeScreen: React.FC = () => {
       }
     }
     fetchHomeData();
-  }, [graphicsFilter])
+  }, [graphicsFilter]);
 
   return (
     <SafeAreaView>
