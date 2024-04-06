f962108ae4dc48397172805af364473f2cd52e2e
Isabelle Hondo <isefshondo@gmail.com>
2024-04-05 23:56:47 -0300

    chore: cow analysis list current main functions

--- src/screens/CowAnalysisListScreen/index.tsx
+++ src/screens/CowAnalysisListScreen/index.tsx
@@ -1,7 +1,34 @@
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {RootStackParams} from '../../../App';
+
+// TODO: Define the correct type
+export type CowAnalysisListDataTypes = ReadonlyArray<any>;
 
 export const CowAnalysisListScreen: React.FC = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParams>>();
+  const [cowAnalysisListData, setCowAnalysisListData] =
+    React.useState<CowAnalysisListDataTypes>();
+
+  const fetchCowAnalysisList = async () => {
+    const res = await fetch('');
+    const data = await res.json();
+    setCowAnalysisListData(data);
+  };
+
+  React.useEffect(() => {
+    fetchCowAnalysisList();
+  }, []);
+
+  const handleCardPress = (id: string) => {
+    navigation.navigate('CowAnalysis', {
+      id,
+    });
+  };
+
   return (
     <SafeAreaView>
       <Text>Cow Analysis List Screen</Text>
