3380c621e27c77233a32c14aecdbbaff4e86181a
Isabelle Hondo <isefshondo@gmail.com>
2024-05-20 21:17:45 -0300

    chore: drawer of Boe

--- src/navigation/RootStack/index.tsx
+++ src/navigation/RootStack/index.tsx
@@ -1,53 +1,58 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {HomeScreen} from '../../screens/HomeScreen';
-import {EditProfileScreen} from '../../screens/EditProfileScreen';
-import {CowAnalysisListScreen} from '../../screens/CowAnalysisListScreen';
-import {CowAnalysisScreen} from '../../screens/CowAnalysisScreen';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {DefaultBottomTab} from '@/components/DefaultBottomTab';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {DefaultBottomTab} from '../../components/DefaultBottomTab';
+import { HomeScreen } from '@/screens/HomeScreen';
+import { CowAnalysisListScreen } from '@/screens/CowAnalysisListScreen';
+import { EditProfileScreen } from '@/screens/EditProfileScreen';
+import { CowAnalysisScreen } from '@/screens/CowAnalysisScreen';
 
-export type HomeStackParams = {
-  MainHome: null;
-  CowAnalysisList: null;
+export type BottomTabsParams = {
+  InitialRoute: null;
+  CowDataListing: null;
 };
-
+export type DrawerTabsParams = {
+  MainRoutes: null;
+  EditProfile: null;
+};
 export type RootStackParams = {
   Home: null;
-  EditProfile: null;
+  CowDetails: null;
   ProcessAnalysisCamera: {id?: string};
   ProcessAnalysisResults: {id?: string};
   ProcessAnalysisRegister: {id?: string};
-  AuthStack: null;
 };
 
-const BottomTab = createBottomTabNavigator<HomeStackParams>();
+const BottomTabs = createBottomTabNavigator<BottomTabsParams>();
+const DrawerTabs = createDrawerNavigator<DrawerTabsParams>();
 const RootStack = createNativeStackNavigator<RootStackParams>();
 
-const DefaultBottomTabBar = props => <DefaultBottomTab {...props} />;
+const MainBottomTab = props => <DefaultBottomTab {...props} />
 
-export function HomeScreensStack(): React.JSX.Element {
+function BottomTabsScreenStack(): React.JSX.Element {
   return (
-    <BottomTab.Navigator
-      initialRouteName="MainHome"
-      screenOptions={{headerShown: false}}
-      tabBar={DefaultBottomTabBar}>
-      <BottomTab.Screen name="MainHome" component={HomeScreen} />
-      <BottomTab.Screen
-        name="CowAnalysisList"
-        component={CowAnalysisListScreen}
-      />
-    </BottomTab.Navigator>
+    <BottomTabs.Navigator initialRouteName='InitialRoute' screenOptions={{headerShown: false}} tabBar={MainBottomTab}>
+      <BottomTabs.Screen name='InitialRoute' component={HomeScreen} />
+      <BottomTabs.Screen name='CowDataListing' component={CowAnalysisListScreen} />
+    </BottomTabs.Navigator>
+  );
+}
+
+function DrawerTabsScreenStack(): React.JSX.Element {
+  return (
+    <DrawerTabs.Navigator>
+      <DrawerTabs.Screen name='MainRoutes' component={BottomTabsScreenStack} />
+      <DrawerTabs.Screen name='EditProfile' component={EditProfileScreen} />
+    </DrawerTabs.Navigator>
   );
 }
 
 export function RootScreensStack(): React.JSX.Element {
   return (
-    <RootStack.Navigator
-      initialRouteName="Home"
-      screenOptions={{headerShown: false}}>
-      <RootStack.Screen name="Home" component={HomeScreensStack} />
-      <RootStack.Screen name="EditProfile" component={EditProfileScreen} />
+    <RootStack.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
+      <RootStack.Screen name='Home' component={DrawerTabsScreenStack} />
+      <RootStack.Screen name='CowDetails' component={CowAnalysisScreen} />
     </RootStack.Navigator>
   );
 }
