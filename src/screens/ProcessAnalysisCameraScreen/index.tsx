e1cc19c59f07c3b8719fb008ce4d5d18049322f1
Isabelle Hondo <isefshondo@gmail.com>
2024-05-25 19:41:03 -0300

    feat: creating request passing image to API

--- src/screens/ProcessAnalysisCameraScreen/index.tsx
+++ src/screens/ProcessAnalysisCameraScreen/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import * as StorageInstance from '../../utils/storage/index.utils';
 import {AnalysisResultsContext} from '@/context/AnalysisResults';
+import {useNavigation} from '@react-navigation/native';
 
 // type NavigationProps = NativeStackScreenProps<>;
 
-export const ProcessAnalysisCameraScreen: React.FC<> = ({route}) => {
+export const ProcessAnalysisCameraScreen: React.FC<NavigationProps> = ({
+  route,
+}) => {
   // Como guardar a foto que eu tirei pelo aplicativo e mandar para a minha API?
+  const navigation = useNavigation();
   const storeCowId = route.params.id ?? null;
   const {setAnalysisResults} = React.useContext(AnalysisResultsContext);
   const [jwt, setJwt] = React.useState<string>('');
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   async function getJWTFromStorage() {
     const loggedInData = await StorageInstance.getFromStorage('loggedInData');
@@ -20,18 +25,21 @@ export const ProcessAnalysisCameraScreen: React.FC<> = ({route}) => {
     getJWTFromStorage();
   }, []);
 
-  // Será chamada por outra função que será chamada quando o usuário clicar no botão da câmera - Nela vai ter o navigate para a tela de Resultados - Vou precisar o Loading
-  async function handleSendPhotoRequest() {
+  async function handleSendPhotoRequest(photo: any) {
+    const data = new FormData();
+    const response = await fetch(photo.uri);
+    const blob = await response.blob();
+    data.append('image', blob, `BOE_PHOTO_${Date.now()}.jpg`);
+
     try {
+      setIsLoading(true);
       const res = await fetch('', {
         method: 'POST',
         headers: {
           Authorization: `Bearer ${jwt}`,
+          'Content-Type': 'multipart/form-data',
         },
-        body: JSON.stringify({
-          cowId: storeCowId,
-          photo: 'base64',
-        }),
+        body: data,
       });
 
       if (!res.ok) {
@@ -40,11 +48,18 @@ export const ProcessAnalysisCameraScreen: React.FC<> = ({route}) => {
         );
       }
 
-      const data = await res.json();
-
-      setAnalysisResults(data);
+      const resData = await res.json();
+      setAnalysisResults(resData);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
+  }
+
+  function handleTakePhotoButton() {
+    // Chamada da função para tirar a foto
+    handleSendPhotoRequest();
+    navigation.navigate('AnalysisResultsScreen', {id: storeCowId});
   }
 };
