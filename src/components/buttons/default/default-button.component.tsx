2b902fa6e7af00ecf544d81bde4cc7ca4d247fd9
Isabelle Hondo <isefshondo@gmail.com>
2024-07-24 22:31:57 -0300

    feat: default button created

---
+++ src/components/buttons/default/default-button.component.tsx
@@ -0,0 +1,26 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+import { DefaultButtonProps } from './types';
+
+export const DefaultButton: React.FC<DefaultButtonProps> = ({
+  children,
+  onPress,
+  icon,
+}) => {
+  function renderIcon() {
+    return (
+      icon && (
+        <>
+          <View style={{ width: 13, height: '100%' }} />
+          {icon}
+        </>
+      )
+    );
+  }
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{children}</Text>
+      {renderIcon()}
+    </TouchableOpacity>
+  );
+};
---
+++ src/components/buttons/default/types.ts
@@ -0,0 +1,5 @@
+export interface DefaultButtonProps {
+  children: string;
+  onPress: () => void;
+  icon?: React.ReactNode;
+}
