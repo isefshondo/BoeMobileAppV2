fd88bef799e9efabcb098d3910fa04ab491adfd3
Isabelle Hondo <isefshondo@gmail.com>
2024-07-24 22:33:48 -0300

    feat: delete button created

---
+++ src/components/buttons/delete/delete-button.component.tsx
@@ -0,0 +1,18 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import { DeleteButtonProps } from './types';
+import { styles } from './styles';
+
+export const DeleteButton: React.FC<DeleteButtonProps> = ({
+  children,
+  onPress,
+  icon,
+}) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>{children}</Text>
+        {icon}
+      </View>
+    </TouchableOpacity>
+  );
+};
