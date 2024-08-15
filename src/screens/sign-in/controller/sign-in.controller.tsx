bb0e1771e5489f6c30f41e714b6c8bfd4ebd6cdd
Isabelle Hondo <isefshondo@gmail.com>
2024-08-14 21:51:05 -0300

    chore: input states created for sign in screen

--- src/screens/sign-in/controller/sign-in.controller.tsx
+++ src/screens/sign-in/controller/sign-in.controller.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import {SignIn} from '../view/sign-in.view';
 
+type SignInInputs = {
+  email: string | null;
+  password: string | null;
+}
+
 export function SignInController() {
+  const [signInInputs, setSignInInputs] = React.useState<SignInInputs>({
+    email: null,
+    password: null,
+  });
+  const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
   return <SignIn />;
 }
